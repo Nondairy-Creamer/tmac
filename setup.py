77190f431c249ac3169d45be6a54199b989eac62
Matthew Creamer <matthew.s.creamer@gmail.com>
2022-03-24 14:44:39 -0400

    update to naming in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1'
-DESCRIPTION = 'Package for inferring latent calcium activity from two-channel imaging'
+VERSION = '0.1.0'
+DESCRIPTION = 'Two-channel motion artifact correction'
 
 # Setting up
 setup(
-    name="calcium_inference",
+    name="tmac",
     version=VERSION,
     author="Matthew S. Creamer",
     author_email="matthew.s.creamer@gmail.com",
@@ -17,7 +17,7 @@ setup(
                       'matplotlib',
                       ],
 
-    keywords=['python', 'calcium', 'inference', 'two-channel', 'imaging'],
+    keywords=['python', 'calcium', 'inference', 'two-channel', 'imaging', 'motion', 'correction'],
     classifiers=[
         "Programming Language :: Python :: 3",
     ]
